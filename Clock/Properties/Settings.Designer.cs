cf701761146b72b72c6d1887fe6ce9c419b6c118
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-12-29 19:02:10 +0300

    1234

--- Clock/Properties/Settings.Designer.cs
+++ Clock/Properties/Settings.Designer.cs
@@ -70,5 +70,17 @@ namespace Clock.Properties {
                 this["my_size_font"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Setting {
+            get {
+                return ((string)(this["Setting"]));
+            }
+            set {
+                this["Setting"] = value;
+            }
+        }
     }
 }
